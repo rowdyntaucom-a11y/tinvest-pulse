6a4b24d02910dc1a39eb7bdb4a370bf5de8bccd1
rowdyntaucom-a11y <rowdyn.taucom@gmail.com>
2026-09-08 13:41:38 +0300

    v7.6.1 register real bond analytics

--- payouts.js
+++ payouts.js
@@ -2,8 +2,10 @@
 
 const registerPayoutCalendar = require('./payouts-core');
 const registerShieldEngine = require('./shield-engine');
+const registerBondAnalytics = require('./bond-analytics');
 
 module.exports = function registerServerModules(app, deps) {
   registerPayoutCalendar(app, deps);
   registerShieldEngine(app, { buildDashboard: deps?.buildDashboard });
+  registerBondAnalytics(app, { tbankRequest: deps?.tbankRequest, buildDashboard: deps?.buildDashboard });
 };
