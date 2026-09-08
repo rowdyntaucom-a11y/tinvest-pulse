67f0e430df4c9513d0697b359f4df9295e91ccdb
rowdyntaucom-a11y <rowdyn.taucom@gmail.com>
2026-09-08 13:59:34 +0300

    v7.7 register What If engine

--- payouts.js
+++ payouts.js
@@ -3,9 +3,11 @@
 const registerPayoutCalendar = require('./payouts-core');
 const registerShieldEngine = require('./shield-engine');
 const registerBondAnalytics = require('./bond-analytics');
+const registerWhatIfEngine = require('./whatif-engine');
 
 module.exports = function registerServerModules(app, deps) {
   registerPayoutCalendar(app, deps);
   registerShieldEngine(app, { buildDashboard: deps?.buildDashboard });
   registerBondAnalytics(app, { tbankRequest: deps?.tbankRequest, buildDashboard: deps?.buildDashboard });
+  registerWhatIfEngine(app, { buildDashboard: deps?.buildDashboard });
 };
