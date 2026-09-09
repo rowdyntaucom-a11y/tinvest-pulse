ccb0c844a3f623552781d65b25e6347d423333a7
rowdyntaucom-a11y <rowdyn.taucom@gmail.com>
2026-09-09 23:04:36 +0300

    publish v11.8 lights and life

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ let html = fs.readFileSync(htmlPath, 'utf8');
 
 // ONE WORLD -> ONE RENDERER -> ONE UPDATE LOOP.
 // v11.3 is the single canvas renderer; later layers add data-driven atmosphere/art only.
-const legacy = /\s*<script\b[^>]*\bsrc\s*=\s*["'][^"']*\/(?:v840|v850|v860|v870|v960|v1021|version-lock|dna-world-v1021-final|dna-world-v1021-single|dna-game-l1|dna-world-polish-v106|dna-game-art-v107|dna-pixel-v108|dna-pixel-v109|dna-game-world-v110|dna-lighting-v111|dna-cinematic-v112|dna-daynight-v113|dna-market-weather-v114|dna-weather-alive-v115|dna-art-detail-v116|dna-environment-v117)\.js(?:\?[^"']*)?["'][^>]*>\s*<\/script>/gi;
+const legacy = /\s*<script\b[^>]*\bsrc\s*=\s*["'][^"']*\/(?:v840|v850|v860|v870|v960|v1021|version-lock|dna-world-v1021-final|dna-world-v1021-single|dna-game-l1|dna-world-polish-v106|dna-game-art-v107|dna-pixel-v108|dna-pixel-v109|dna-game-world-v110|dna-lighting-v111|dna-cinematic-v112|dna-daynight-v113|dna-market-weather-v114|dna-weather-alive-v115|dna-art-detail-v116|dna-environment-v117|dna-lights-life-v118)\.js(?:\?[^"']*)?["'][^>]*>\s*<\/script>/gi;
 html = html.replace(legacy, '');
-html = html.replace('</body>', '<script src="/dna-daynight-v113.js?build=1170-environment"></script><script src="/dna-market-weather-v114.js?build=1170-environment"></script><script src="/dna-weather-alive-v115.js?build=1170-environment"></script><script src="/dna-art-detail-v116.js?build=1170-environment"></script><script src="/dna-environment-v117.js?build=1170-environment"></script></body>');
+html = html.replace('</body>', '<script src="/dna-daynight-v113.js?build=1180-lights-life"></script><script src="/dna-market-weather-v114.js?build=1180-lights-life"></script><script src="/dna-weather-alive-v115.js?build=1180-lights-life"></script><script src="/dna-art-detail-v116.js?build=1180-lights-life"></script><script src="/dna-environment-v117.js?build=1180-lights-life"></script><script src="/dna-lights-life-v118.js?build=1180-lights-life"></script></body>');
 fs.writeFileSync(htmlPath, html);
-process.env.TINVEST_BUILD = '11.7.0';
+process.env.TINVEST_BUILD = '11.8.0';
 
 require('./server-core.js');
