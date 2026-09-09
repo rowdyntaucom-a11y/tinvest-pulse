39a19ece8f27196497f1d955768e8ac3fc6ba380
rowdyntaucom-a11y <rowdyn.taucom@gmail.com>
2026-09-09 06:14:09 +0300

    v7.13.2 register clean analytics endpoint

--- payouts.js
+++ payouts.js
@@ -4,10 +4,12 @@ const registerPayoutCalendar = require('./payouts-core');
 const registerShieldEngine = require('./shield-engine');
 const registerBondAnalytics = require('./bond-analytics');
 const registerWhatIfEngine = require('./whatif-engine');
+const registerAnalyticsIntegrity = require('./analytics-integrity');
 
 module.exports = function registerServerModules(app, deps) {
   registerPayoutCalendar(app, deps);
   registerShieldEngine(app, { buildDashboard: deps?.buildDashboard });
+  registerAnalyticsIntegrity(app, { buildDashboard: deps?.buildDashboard });
   const bondDeps={ tbankRequest: deps?.tbankRequest, buildDashboard: deps?.buildDashboard };
   registerBondAnalytics(app, bondDeps);
   registerWhatIfEngine(app, { buildDashboard: deps?.buildDashboard, getBondDuration: (...args)=>bondDeps.getBondDuration?.(...args) });
