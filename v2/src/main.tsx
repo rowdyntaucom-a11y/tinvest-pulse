a939400401cda716209adfe829efaed0de8853a6
rowdyntaucom-a11y <rowdyn.taucom@gmail.com>
2026-09-11 16:53:06 +0300

    QVANIX v2 · load drift styles

--- v2/src/main.tsx
+++ v2/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles.css'
+import './features/analytics/drift.css'
 import './final-shell.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
