da5ddb5fda82f33cfbe14583c3119b4381f44275
rowdyntaucom-a11y <rowdyn.taucom@gmail.com>
2026-09-11 17:22:34 +0300

    QVANIX v2 · load Monte Carlo styles

--- v2/src/main.tsx
+++ v2/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './features/analytics/drift.css'
+import './features/analytics/monteCarlo.css'
 import './final-shell.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
