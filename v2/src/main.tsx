cc348e5d9133040095076b69eb4312a7715640e1
rowdyntaucom-a11y <rowdyn.taucom@gmail.com>
2026-09-14 10:09:17 +0300

    QVANIX: load data fill motion layer

--- v2/src/main.tsx
+++ v2/src/main.tsx
@@ -10,6 +10,7 @@ import './features/board/boardShell.css'
 import './features/settings/boardPersonalization.css'
 import './final-shell.css'
 import './ambientShell.css'
+import './dataFillMotion.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
