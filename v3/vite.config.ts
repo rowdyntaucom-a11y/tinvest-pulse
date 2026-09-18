44f0d413399e213b47fab82a24569256dce8e9a1
rowdyntaucom-a11y <rowdyn.taucom@gmail.com>
2026-09-18 15:23:48 +0300

    V3 runtime: v3/vite.config.ts

---
+++ v3/vite.config.ts
@@ -0,0 +1,1 @@
+import{defineConfig}from"vite";import react from"@vitejs/plugin-react";export default defineConfig({base:"./",plugins:[react()],server:{host:true,proxy:{"/api":"http://localhost:10000"}},build:{target:"es2022"}});
